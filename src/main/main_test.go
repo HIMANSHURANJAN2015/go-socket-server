76ed8645789aed76802199bb3a2061723a4473b1
Himanshu <livelikehimanshu12@gmail.com>
2018-03-07 13:03:04 +0530

    Modified project structure to include relative import and using single GOPATH for all my projects.

---
+++ src/main/main_test.go
@@ -0,0 +1,20 @@
+package main 
+
+import "testing"
+
+// func Test(t *testing.T) {
+//   var v float64
+//   v = Average([]float64{1,2})
+//   if v != 1.5 {
+//     t.Error("Expected 1.5, got ", v)
+//   }
+// }
+
+func TestSab(t *testing.T) {
+	t.Log("himanshu")
+	// start server
+	// go main()
+	// 
+	// Making requests to server 1) /ws 2) /ws/store 3) /ws/store?storeId=21 4) /ws/store?storeId=25
+	
+}
